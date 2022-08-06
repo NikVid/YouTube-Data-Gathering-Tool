6654fe9ea137093f7fcd50259625fa75ca2c10c5
Nika <nika.vidic777@gmail.com>
2022-08-06 13:49:49 +0200

    First version
    1 hour of work

---
+++ app.js
@@ -0,0 +1,335 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
+const {
+  google
+} = require("googleapis");
+const _ = require("lodash");
+
+const {
+  convertArrayToCSV
+} = require('convert-array-to-csv');
+let videoIDs = [];
+const converter = require('convert-array-to-csv');
+const {
+  creatCsvFile,
+  downloadFile,
+  detectionClientType
+} = require("download-csv");
+const convertCsvToXlsx = require('@aternus/csv-to-xlsx');
+const fs = require("fs");
+const path = require('path');
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+//
+async function download(data, searchQuery, res) {
+  console.log(data);
+
+  
+  res.header('Content-Type', 'text/csv');
+  res.attachment("Youtube_Data_" + searchQuery + ".csv");
+  res.send(data);
+}
+//
+
+
+const csv = ["Video Title", "Thumbnail", "Channel Title","Video Description", "Publish Date","Url", "Views", "Likes", "Comments", "Channel Description", "Categories","Language","Tags","Duration"];
+const videoUrl = "https://www.youtube.com/watch?v=";
+let totalResults;
+let foutcode;
+let loadedPages = [];
+let resultsPerPage;
+let currentPage = 0;
+let size;
+let result = 0;
+let tokenList = [];
+let nextPageToken = "";
+let channelObject;
+let searchObject;
+let videoObject;
+let dataObject;
+let maxVisibleRequests;
+let maxKeys;
+
+let searchQuery;
+
+// const apiKey = "AIzaSyBS699Q19K70lVJ1zzKlaE_NU4zF8IpvZE";
+// const apiKey = "AIzaSyD0WGLPPV_2m9ZQpe07PlzY8pg6KlmU0HU";
+// const apiKey = "AIzaSyBD0eqh2x-H0XCxT0jicpNVTuBwsTOHw4Q";
+// const apiKey = "AIzaSyAcyfNE1hvPTkZsATNggbaNIsh2yh-HOi0";
+// const apiKey = "AIzaSyAYqWAY34gE8kGlBz6fdItByDabY_m72q0";
+const apiKey= "AIzaSyC2R6kmg26jBj9ufwKn25-TstyKszFv-Z4";
+const apiUrl = "https://www.googleapis.com/youtube/v3";
+
+const youtubeSearch = google.youtube({
+  version: "v3",
+  auth: apiKey
+});
+
+app.get("/", function(req, res) {
+  res.render("home", {});
+
+
+
+  // for(let u=0;u<loadedPages.length;u++){
+  //
+  // }
+
+
+});
+
+app.post("/", function(req, res) {
+  searchQuery = req.body.searchQuery;
+  resultsPerPage = req.body.results;
+  maxVisibleRequests = req.body.maximum_results;
+  console.log(searchQuery, resultsPerPage);
+
+  res.redirect("/search");
+});
+
+
+app.get("/search", async function(req, res, next) {
+  try {
+    const searchResponse = await youtubeSearch.search.list({
+      part: "id, snippet",
+      q: searchQuery,
+      type: "video",
+      maxResults: resultsPerPage
+    });
+    videoIDs = searchResponse.data?.items.map((item) => item.id.videoId);
+    const channelIDs = searchResponse.data?.items.map((item) => item.snippet.channelId);
+    searchObject = searchResponse.data?.items;
+    nextPageToken = searchResponse.data?.nextPageToken;
+    totalResults = searchResponse.data?.pageInfo.totalResults;
+    try {
+      const videoResponse = await youtubeSearch.videos.list({
+        part: "snippet, statistics, topicDetails, contentDetails",
+        id: videoIDs.join(",")
+      });
+      videoObject = videoResponse.data?.items;
+    } catch (err) {
+      res.redirect("/search/error/" + encodeURIComponent(err.message));
+      return;
+    }
+    try {
+      const channelResponse = await youtubeSearch.channels.list({
+        part: "snippet",
+        id: channelIDs.join(",")
+      });
+      channelObject = channelResponse.data?.items ?? [];
+    } catch (err) {
+
+      res.redirect("/search/error/" + encodeURIComponent(err.message));
+      return;
+    }
+    // loadedPages.push([ searchObject?.snippet?.title, searchObject?.snippet?.thumbnails?.high?.url, searchObject?.snippet?.channelTitle,searchObject?.snippet?.description,searchObject?.snippet?.publishTime,videoObject?.statistics?.viewCount,videoObject?.statistics?.likeCount,videoObject?.statistics?.commentCount,channelObject?.snippet?.description,videoObject?.topicDetails?.topicCategories,videoObject?.snippet?.defaultAudioLanguage,videoObject?.snippet?.tags,videoObject?.contentDetails?.duration]);
+
+  } catch (err) {
+    res.redirect("/search/error/" + encodeURIComponent(err.message));
+    return;
+  }
+  // Belangrijk om te weten of de loadedpages push goed staat ivm try catch, ook goed om te weten of het komt als een loadedPages[result][searchObject][videoObject][channelObject]
+
+  tokenList.push(nextPageToken);
+  size = resultsPerPage;
+  if (totalResults < maxVisibleRequests) {
+    maxVisibleRequests = totalResults;
+
+  }
+  maxKeys = maxVisibleRequests / resultsPerPage;
+  console.log("maxKeys", maxKeys, "maxVisibleRequests", maxVisibleRequests, "resultsPerPage", resultsPerPage);
+  try {
+
+    for (let i = 0; i < maxKeys; i++) {
+      searchResponse = await youtubeSearch.search.list({
+        part: "id, snippet",
+        q: searchQuery,
+        type: "video",
+        pageToken: tokenList[i],
+        maxResults: resultsPerPage
+      });
+      tokenList.push(searchResponse.data.nextPageToken);
+    }
+    console.log("tokenlist", tokenList);
+  } catch (err) {
+    res.redirect("/search/error/" + encodeURIComponent(err.message));
+
+  }
+  console.log(videoIDs);
+  res.render("resultPage", {
+    size: size,
+    searchObject: searchObject,
+    videoObject: videoObject,
+    channelObject: channelObject,
+    nextToken: tokenList,
+    currentPage: currentPage,
+    videoIds: videoIDs,
+    videoUrl: videoUrl
+  });
+});
+
+
+app.get("/search/:key", async function(req, res) {
+  let currentKey = req.params.key;
+  console.log("current key", currentKey);
+
+  result = _.findIndex(tokenList, (token) => token === currentKey);
+  console.log("current page", result);
+  if (result < loadedPages.length) {
+    console.log("paginas geladen tot nu:", loadedPages.length);
+    res.render("resultPage", {
+      size: size,
+      searchObject: loadedPages[result][0],
+      videoObject: loadedPages[result][1],
+      channelObject: loadedPages[result][2],
+      currentPage: result,
+      nextToken: tokenList
+    });
+  } else {
+    try {
+      const searchResponse = await youtubeSearch.search.list({
+        part: "id, snippet",
+        q: searchQuery,
+        type: "video",
+        pageToken: tokenList[result]
+      });
+      const videoIDs = searchResponse.data?.items.map((item) => item.id.videoId);
+      const channelIDs = searchResponse.data?.items.map((item) => item.snippet.channelId);
+      searchObject = searchResponse.data?.items;
+      nextPageToken = searchResponse.data?.nextPageToken;
+      try {
+        const videoResponse = await youtubeSearch.videos.list({
+          part: "snippet, statistics, topicDetails, contentDetails",
+          id: videoIDs.join(",")
+        });
+        videoObject = videoResponse.data?.items;
+      } catch (err) {
+        res.redirect("/search/error/" + encodeURIComponent(err.message));
+      }
+      try {
+        const channelResponse = await youtubeSearch.channels.list({
+          part: "snippet",
+          id: channelIDs.join(",")
+        });
+        channelObject = channelResponse.data?.items;
+      } catch (err) {
+        res.redirect("/search/error/" + encodeURIComponent(err.message));
+        return;
+      }
+    } catch (err) {
+      res.redirect("/search/error/" + encodeURIComponent(err.message));
+      return;
+    }
+
+    loadedPages.push([searchObject, videoObject, channelObject]);
+    res.render("resultPage", {
+      size: size,
+      searchObject: searchObject,
+      videoObject: videoObject,
+      channelObject: channelObject,
+      currentPage: result,
+      nextToken: tokenList
+    });
+  }
+});
+
+app.get("/search/error/:error", function(req, res) {
+  foutcode = req.params.error;
+  res.render("error", {
+    foutcode: foutcode
+  })
+});
+app.get("/about", function(req, res) {
+
+  res.render("", )
+});
+app.get("/download:keyword", async function(req, res) {
+  console.log("redirecten is gelukt");
+
+
+  console.log("loadedPages.length", loadedPages.length);
+  if (loadedPages.length < totalResults) {
+    console.log("we zitten in de if");
+    console.log("loadedPagesg length", loadedPages.length);
+    for (let o = loadedPages.length; o < 20; o++) {
+      console.log("we zitten in de for loop");
+      try {
+        const searchResponse = await youtubeSearch.search.list({
+          part: "id, snippet",
+          q: searchQuery,
+          type: "video",
+          maxResults: resultsPerPage,
+          nextToken: tokenList[o]
+        });
+        const videoIDs = searchResponse.data?.items.map((item) => item.id.videoId);
+        const channelIDs = searchResponse.data?.items.map((item) => item.snippet.channelId);
+        searchObject = searchResponse.data?.items;
+        nextPageToken = searchResponse.data?.nextPageToken;
+        try {
+          const videoResponse = await youtubeSearch.videos.list({
+            part: "snippet, statistics, topicDetails, contentDetails",
+            id: videoIDs.join(",")
+          });
+          videoObject = videoResponse.data?.items;
+          // console.log("videoobject", videoObject);
+        } catch (err) {
+          console.log("err1");
+          res.redirect("/search/error/" + encodeURIComponent(err.message));
+          return;
+        }
+        try {
+          const channelResponse = await youtubeSearch.channels.list({
+            part: "snippet",
+            id: channelIDs.join(",")
+          });
+          channelObject = channelResponse.data?.items ?? [];
+        } catch (err2) {
+          console.log("err2");
+          res.redirect("/search/error/" + encodeURIComponent(err.message));
+          return;
+        }
+        // console.log("push naar loadedPages");
+        loadedPages.push([searchObject[o]?.snippet?.title || "", searchObject[o]?.snippet?.thumbnails?.high?.url || "", searchObject[o]?.snippet?.channelTitle || "", searchObject[o]?.snippet?.description || "", searchObject[o]?.snippet.publishTime || "", videoUrl + videoIDs[o], videoObject[o]?.statistics.viewCount || "", videoObject[o]?.statistics.likeCount || "", videoObject[o]?.statistics?.commentCount || "disabled", channelObject[o]?.snippet?.description || "", videoObject[o]?.topicDetails?.topicCategories?.join(",") || "", videoObject[o]?.snippet?.defaultAudioLanguage || "not available", videoObject[o]?.snippet?.tags?.join(",") || "", videoObject[o]?.contentDetails?.duration || ""]);
+
+      } catch (err) {
+        console.log(err);
+      }
+
+    }
+
+  }
+  // loadedPages = loadedPages.map(item=>{
+  //    if(!(typeof item==="object")){
+  //     return item
+  //   }
+  //   return item.join(",")
+  // })
+
+  console.log("loadedPages", loadedPages);
+
+  const csvFromArrayOfArrays = await convertArrayToCSV([csv, ...loadedPages], {
+    csv,
+    separator: ';'
+  });
+  // console.log("csv data", csvFromArrayOfArrays);
+  download(csvFromArrayOfArrays, searchQuery, res);
+
+  // for(let u=0;u<loadedPages.length;u++){
+  //
+  // }
+});
+
+
+app.listen(3000, function() {
+  console.log("app running on port 3000");
+});
+app.post("/download", function(req, res) {
+  console.log("redirecten naar downloads");
+  res.redirect("/download" + searchQuery);
+});
---
+++ data.js
@@ -0,0 +1,230 @@
+export default [
+  'Jordan&#39;s Boob Slip Caused By Peter | I&#39;m A Celebrity... Get Me Out Of Here!',
+  'https://i.ytimg.com/vi/NIqfBou2tUA/hqdefault.jpg',
+  "I'm A Celebrity... Get Me Out Of Here!",
+  "Peter is massaging Jordan's back when all of a sudden her top falls down. Watch the episodes on ITV Hub: ...",
+  '2014-11-10T09:42:31Z',
+  'https://www.youtube.com/watch?v=NIqfBou2tUA',
+  '37747577',
+  '54152',
+  'disabled',
+  'Opening Eyes. Speaking Truths. Changing Minds.\n' +
+    '\n' +
+    'Check out the new SI Swimsuit 2022 issue HERE: https://swimsuit.si.com/\n' +
+    '\n' +
+    'Follow us:\n' +
+    'IG: @SI_SWIMSUIT\n' +
+    'TikTok: @SportsIllustratedSwim\n' +
+    '\n',
+  [
+    'https://en.wikipedia.org/wiki/Entertainment',
+    'https://en.wikipedia.org/wiki/Television_program'
+  ],
+  'en-US',
+  [
+    'Kerry Katona',
+    'Jennie Bond',
+    'Peter Andre',
+    'Lord Brocket',
+    'Katie Price',
+    'Jordan',
+    'Alex Best',
+    'John Lydon',
+    'Johnny Rotten',
+    'Neil Ruddock',
+    'Diane Modahl',
+    'Mike Read',
+    "I'm a Celebrity Get Me Out Of Here",
+    'Ant & Dec',
+    "i'm a celebrity 2020",
+    'im a celeb 2020'
+  ],
+  'PT2M3S',
+  'Those Boobs | Robot Chicken | Adult Swim',
+  'https://i.ytimg.com/vi/2O6GKaekZ0M/hqdefault.jpg',
+  'Adult Swim',
+  "A classic retelling of Tarzan's first meeting with Boobs. I mean Jane. SUBSCRIBE: http://bit.ly/AdultSwimSubscribe About Robot ...",
+  '2013-04-10T20:00:07Z',
+  'https://www.youtube.com/watch?v=2O6GKaekZ0M',
+  '22184688',
+  '124159',
+  '3140',
+  'Watch Adult Swim on HBO Max, www.adultswim.com, or by downloading the Adult Swim app. Binge marathons or watch selected episodes of many of your favorite shows including Rick and Morty, Robot Chicken, Venture Bros., Aqua Teen Hunger Force and many more.',
+  [
+    'https://en.wikipedia.org/wiki/Entertainment',
+    'https://en.wikipedia.org/wiki/Film'
+  ],
+  'en',
+  [
+    'Seth Green',
+    'adult swim song',
+    'Robot Chicken',
+    'mike lazzo',
+    'adult swim shows',
+    'Stop Motion',
+    'Robot Chicken Batmans',
+    'adultswim',
+    'stoopid monkey',
+    'Robot Chicken Gummy Bear',
+    'Robot Chicken Star Wars',
+    'Robot Chicken Full Episodes',
+    'Parody',
+    'Those Boobs',
+    'Robot Chicken Spongebob',
+    'Animation',
+    'matthew senreich',
+    'Funny',
+    'Adult Swim',
+    'adult swim music',
+    'adult swim instrumentals',
+    'adult swim robot chicken',
+    'Comedy',
+    'adult swim cartoon'
+  ],
+  'PT2M40S',
+  '18 Big BOOBS Struggles | Smile Squad Comedy',
+  'https://i.ytimg.com/vi/DtMYyrfKcq0/hqdefault.jpg',
+  'Smile Squad Comedy',
+  'How many times does no. 14 happen?! Follow us on Instagram! Markian - @markianb https://www.instagram.com/markianb/ ...',
+  '2020-03-09T19:00:00Z',
+  'https://www.youtube.com/watch?v=DtMYyrfKcq0',
+  '13340416',
+  '69643',
+  '3333',
+  "Smile Squad's mission is to bring the world together, one smile at a time! \n" +
+    '\n' +
+    'On this comedy channel, we create light-hearted, relatable skits that celebrate different communities. We focus on representing identities such as culture, body types, personality traits, relationships... we hope we can brighten your day!\n' +
+    '\n' +
+    'Business Inquiries: business@markianb.com\n' +
+    '\n' +
+    'Smile and the world will smile back :)\n',
+  [
+    'https://en.wikipedia.org/wiki/Lifestyle_(sociology)',
+    'https://en.wikipedia.org/wiki/Technology'
+  ],
+  'en',
+  [
+    'markian',
+    'smile',
+    'squad',
+    'skits',
+    'comedy',
+    'relatable',
+    'funny',
+    'markianb',
+    'markian benhamou',
+    'smile squad',
+    'markain',
+    'markian smile squad',
+    'smile media',
+    'markian smile',
+    'how to',
+    'types of',
+    'international',
+    'boobs',
+    '18 Big BOOBS Struggles',
+    'big boobs',
+    'boob strugges',
+    'D cup',
+    'big boob struggles'
+  ],
+  'PT2M6S',
+  'Jan&#39;s Boob Job  - The Office US',
+  'https://i.ytimg.com/vi/fx9-cMoEv7k/hqdefault.jpg',
+  'The Office',
+  `Aren't they just emotionally magnificent??? Season 3, Episode 24 "The Job" - Michael, Jim and Karen go to New York to interview ...`,
+  '2020-01-28T19:00:05Z',
+  'https://www.youtube.com/watch?v=fx9-cMoEv7k',
+  '16163234',
+  '125933',
+  '4403',
+  "Smile Squad's mission is to bring the world together, one smile at a time! \n" +
+    '\n' +
+    'On this comedy channel, we create light-hearted, relatable skits that celebrate different communities. We focus on representing identities such as culture, body types, personality traits, relationships... we hope we can brighten your day!\n' +
+    '\n' +
+    'Business Inquiries: business@markianb.com\n' +
+    '\n' +
+    'Smile and the world will smile back :)\n',
+  [
+    'https://en.wikipedia.org/wiki/Entertainment',
+    'https://en.wikipedia.org/wiki/Film',
+    'https://en.wikipedia.org/wiki/Television_program'
+  ],
+  'en',
+  [
+    'the office',
+    'the office full episodes',
+    'rainn wilson',
+    'john krasinski',
+    'steve carell',
+    'michael scott',
+    'the office fire drill',
+    'jim and dwight pranks',
+    'dwight schrute',
+    'jim halpert',
+    'jenna fischer',
+    'the office thug life',
+    'the office funniest moments',
+    'the office bloopers season 1',
+    'the office cpr',
+    'the office parkour',
+    'Best The Office Moments',
+    'Jan Levinson',
+    'Melora Hardin',
+    'Boob Job',
+    'Michael and Jan',
+    'Season 3',
+    'The Job',
+    'Swing low sweet chariots'
+  ],
+  'PT3M39S',
+  'Jan&#39;s Boob Job  - The Office US',
+  'https://i.ytimg.com/vi/fx9-cMoEv7k/hqdefault.jpg',
+  'The Office',
+  `Aren't they just emotionally magnificent??? Season 3, Episode 24 "The Job" - Michael, Jim and Karen go to New York to interview ...`,
+  '2020-01-28T19:00:05Z',
+  'https://www.youtube.com/watch?v=fx9-cMoEv7k',
+  '16163234',
+  '125933',
+  '4403',
+  "Smile Squad's mission is to bring the world together, one smile at a time! \n" +
+    '\n' +
+    'On this comedy channel, we create light-hearted, relatable skits that celebrate different communities. We focus on representing identities such as culture, body types, personality traits, relationships... we hope we can brighten your day!\n' +
+    '\n' +
+    'Business Inquiries: business@markianb.com\n' +
+    '\n' +
+    'Smile and the world will smile back :)\n',
+  [
+    'https://en.wikipedia.org/wiki/Entertainment',
+    'https://en.wikipedia.org/wiki/Film',
+    'https://en.wikipedia.org/wiki/Television_program'
+  ],
+  'en',
+  [
+    'the office',
+    'the office full episodes',
+    'rainn wilson',
+    'john krasinski',
+    'steve carell',
+    'michael scott',
+    'the office fire drill',
+    'jim and dwight pranks',
+    'dwight schrute',
+    'jim halpert',
+    'jenna fischer',
+    'the office thug life',
+    'the office funniest moments',
+    'the office bloopers season 1',
+    'the office cpr',
+    'the office parkour',
+    'Best The Office Moments',
+    'Jan Levinson',
+    'Melora Hardin',
+    'Boob Job',
+    'Michael and Jan',
+    'Season 3',
+    'The Job',
+    'Swing low sweet chariots'
+  ],
+  'PT3M39S'
+];
